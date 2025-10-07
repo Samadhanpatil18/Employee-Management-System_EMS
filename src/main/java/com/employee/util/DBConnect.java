93a691a47c225ead36d9bc98cfb8182f9728faf7
samadhan0018 <samadhan0018@gmail.com>
2025-10-07 17:11:46 +0530

    Ignore database config file

--- src/main/java/com/employee/util/DBConnect.java
+++ src/main/java/com/employee/util/DBConnect.java
@@ -6,12 +6,11 @@ import java.sql.SQLException;
 
 public class DBConnect {
 
-	 	private static final String URL = "jdbc:mysql://localhost:3306/sam1";
-	    private static final String USERNAME = "root";
-	    private static final String PASSWORD = "Samadhan@123";
-	    
-	  
-
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/my_new_app_db";
+	private static final String USERNAME = "my_app_user";
+	private static final String PASSWORD = "Sam@2003";
+	
 	static {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
